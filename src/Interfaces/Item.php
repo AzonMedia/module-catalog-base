4a73718f6ba8fa467faa735d83d6491931cd1d2a
Vesko Kenashkov <vesko@azonmedia.com>
2020-10-06 12:48:40 +0300

    wip

--- src/Interfaces/Item.php
+++ src/Interfaces/Item.php
@@ -7,9 +7,5 @@ use GuzabaPlatform\Tags\Base\Interfaces\SupportsTagsInterface;
 
 interface Item extends SupportsTagsInterface
 {
-    /**
-     * A collection/array of Category
-     * @return iterable
-     */
-    public function get_categories() : iterable ;
+
 }
